b23385f1c951ebe6eb08c194bdf355af18bddf99
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:12:04 -0600

    add services for review areas

---
+++ src/api/reviewArea.js
@@ -0,0 +1,30 @@
+import fetchApi from "./fetchApi"
+
+const fetchReviewAreas = () => fetchApi({
+  url: '/area-revisora/',
+  method: 'GET',
+})
+
+const fetchReviewAreaById = (id) => fetchApi({
+  url: `/area-revisora/${id}/`,
+  method: 'GET',
+})
+
+const storeReviewArea = (reviewArea) => fetchApi({
+  url: '/area-revisora/',
+  method: 'POST',
+  data: reviewArea,
+})
+
+const updateReviewArea = (reviewArea) => fetchApi({
+  url: `/area-revisora/${reviewArea.clave_unidad}/`,
+  method: 'PUT',
+  data: reviewArea,
+})
+
+export {
+  fetchReviewAreas,
+  fetchReviewAreaById,
+  storeReviewArea,
+  updateReviewArea,
+}
