f7e21e9f4ca00176366fca7703627416e0141ec2
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:08:54 -0600

    add review area index view

---
+++ src/views/ReviewArea/Index.vue
@@ -0,0 +1,73 @@
+<template>
+  <main class="px-4 mt-10">
+    <arrow-back />
+    <title-bar title="Catálogos" subtitle="Áreas revisoras" />
+    <section class="px-4">
+      <button-base label="Nueva área revisora" @click="goToNewReviewArea" class="mb-3 mr-0 ml-auto" />
+      <table-base
+        :options="featureOptions"
+        :headers="headers"
+        :data="reviewAreas"
+      />
+    </section>
+  </main>
+</template>
+
+<script>
+import { ref } from 'vue'
+import TableBase from '../../components/TableBase.vue'
+import { fetchReviewAreas } from './../../api/reviewArea'
+import ArrowBack from '../../components/ArrowBack.vue'
+import ButtonBase from '../../components/ButtonBase.vue'
+import Breadcrumb from '../../components/Breadcrumb.vue'
+import { useRouter } from 'vue-router'
+import TitleBar from '../../components/TitleBar.vue'
+
+export default {
+  name: 'ReviewAreasIndex',
+  components: {
+    TableBase,
+    ArrowBack,
+    ButtonBase,
+    Breadcrumb,
+    TitleBar,
+  },
+  setup() {
+    const router = useRouter()
+    const headers = [
+      {
+        label: 'Nombre unidad',
+        field: 'nombre_unidad',
+      },
+      {
+        label: 'Estatus',
+        field: 'estado_area_revisora',
+      },
+      {
+        label: 'Fecha inicio',
+        field: 'fecha_inicio_area_revisora',
+      },
+      {
+        label: 'Fecha fin',
+        field: 'fecha_fin_area_revisora',
+      },
+    ]
+    const reviewAreas = ref([])
+    const getReviewAreas = async () => {
+      const { data } = await fetchReviewAreas()
+      reviewAreas.value = data
+    }
+    const featureOptions = []
+    const goToNewReviewArea = () => router.push({ name: 'NewReviewArea' })
+
+    getReviewAreas()
+
+    return {
+      reviewAreas,
+      featureOptions,
+      headers,
+      goToNewReviewArea,
+    }
+  },
+}
+</script>
