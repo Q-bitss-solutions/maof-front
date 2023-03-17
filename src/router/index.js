c69f5cbb90ab11b390bba2e87eed7319ca7b1ce0
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:05:03 -0600

    add routes for contracts, projects and review areas

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,51 @@ const routes = [
     name: 'Home',
     component: () => import('./../views/Home.vue'),
   },
+  {
+    path: '/proyectos',
+    name: 'Projects',
+    component: () => import('./../views/Project/Index.vue'),
+  },
+  {
+    path: '/proyectos/nuevo',
+    name: 'NewProject',
+    component: () => import('./../views/Project/New.vue'),
+  },
+  {
+    path: '/proyectos/:projectId',
+    name: 'EditProject',
+    component: () => import('./../views/Project/Edit.vue'),
+  },
+  {
+    path: '/contratos',
+    name: 'Contracts',
+    component: () => import('./../views/Contract/Index.vue'),
+  },
+  {
+    path: '/contratos/nuevo',
+    name: 'NewContract',
+    component: () => import('./../views/Contract/New.vue'),
+  },
+  {
+    path: '/contratos/:contractId',
+    name: 'EditContract',
+    component: () => import('./../views/Contract/Edit.vue'),
+  },
+  {
+    path: '/areas-revisoras',
+    name: 'ReviewAreas',
+    component: () => import('./../views/ReviewArea/Index.vue'),
+  },
+  {
+    path: '/areas-revisoras/nuevo',
+    name: 'NewReviewArea',
+    component: () => import('./../views/ReviewArea/New.vue'),
+  },
+  {
+    path: '/areas-revisoras/:reviewAreaId',
+    name: 'EditReviewArea',
+    component: () => import('./../views/ReviewArea/Edit.vue'),
+  },
 ]
 
 const router = createRouter({
