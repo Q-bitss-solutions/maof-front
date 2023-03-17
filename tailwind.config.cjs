f41599cf87816bdd88250e9e934cbf327f5d335d
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:15:44 -0600

    add color palette to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'red': '#9d2449',
+      'black': '#000000',
+      'white': '#ffffff',
+      'gray': '#cccccc',
+      'gray-50': '#f5f5f5',
+      'gray-100': '#dddddd',
+      'gray-200': '#DCE0E0',
+      'gray-900': '#404041',
+      'gray-800': '#545454',
+      'gray-600': '#6F7271',
+      'blue': '#66afe9',
+      'brown-300': '#B38E5D',
+    },
   },
   plugins: [],
 }
