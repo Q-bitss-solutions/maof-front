8f283d310022fe1c3dafd8f673002a580348c091
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:08:32 -0600

    add project edit view

---
+++ src/views/Project/Edit.vue
@@ -0,0 +1,53 @@
+<template>
+  <main class="px-4 mt-10">
+    <arrow-back />
+    <title-bar title="Catálogos" subtitle="Proyectos" />
+    <section class="px-4">
+      <form-project @submit="saveProject" :project="app.project" edit-mode v-if="!app.loading" />
+    </section>
+  </main>
+</template>
+
+<script>
+import FormProject from '../../components/Project/FormProject.vue'
+import ArrowBack from '../../components/ArrowBack.vue'
+import TitleBar from '../../components/TitleBar.vue'
+import { updateProject, fetchProjectById } from '../../api/project'
+import { reactive } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+export default {
+  name: 'EditProject',
+  components: {
+    FormProject,
+    ArrowBack,
+    TitleBar,
+  },
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const app = reactive({
+      project: {},
+      loading: true,
+    })
+    const getProjectById = async () => {
+      app.loading = true
+      const { data } = await fetchProjectById(route.params.projectId)
+      app.project = data
+      app.loading = false
+    }
+    const saveProject = async (project) => {
+      await updateProject(project)
+      alert('Proyecto actualizado con exito!')
+      router.push({ name: 'Projects' })
+    }
+
+    getProjectById()
+
+    return {
+      app,
+      saveProject,
+    }
+  },
+}
+</script>
