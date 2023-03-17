0882c7ac7697089fd1ecce4b265b9fea87cc5827
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:00:49 -0600

    add table base web component

---
+++ src/components/TableBase.vue
@@ -0,0 +1,93 @@
+<template>
+  <table class="border border-solid border-gray-100 border-collapse text-gray-900 w-full">
+    <tr>
+      <th
+        v-for="(header, index) in headers"
+        :key="index"
+        class="p-2 border border-b-2 border-solid border-gray-100"
+      >
+        {{ header.label }}
+      </th>
+    </tr>
+    <tr
+      id="rows"
+      class=""
+      v-for="(item, index) in data"
+      :key="index"
+    >
+      <td
+        v-for="(header, index) in headers"
+        :key="index"
+        class="p-2 border border-solid border-gray-100"
+      >
+        {{ item[header.field] }}
+      </td>
+      <td v-if="options.length" class="p-2 border border-solid border-gray-100 relative">
+        <div class="flex justify-center">
+          <div class="p-1 cursor-pointer" @click="openActions(`table-actions-${index}`)">
+            <span
+              v-for="(dot, index) in dots"
+              :key="index"
+              class="rounded-full h-1 w-1 block bg-blue mb-1"
+            />
+          </div>
+        </div>
+        <div class="hidden absolute right-4 top-8 bg-white z-10" style="box-shadow: -3px 3px 6px #00000029;" :id="`table-actions-${index}`" @mouseleave="openActions(`table-actions-${index}`)">
+          <div class="flex flex-col">
+            <div v-for="(option, index) in options" :key="index" class="h-8 flex justify-center items-center py-2 px-8">
+              <p
+                class="text-xs border-b border-solid border-gray-100 cursor-pointer hover:border-gray hover:font-medium"
+                @click="option.action(item)"
+              >
+                {{ option.label }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TableBase',
+  props: {
+    headers: {
+      type: Array,
+      default: () => ([
+        {
+          label: 'Example Header',
+          field: 'example'
+        }
+      ]),
+    },
+    data: {
+      type: Array,
+      default: () => ([
+        {
+          example: 'value 1',
+        }
+      ]),
+    },
+    options: {
+      type: Array,
+      default: () => ([
+        {
+          label: 'example',
+          action: (context) => console.log(context),
+        }
+      ]),
+    },
+  },
+  setup() {
+    const dots = [1, 2, 3]
+    const openActions = (id) => {
+      document.getElementById(id).classList.toggle('hidden')
+    }
+
+    return { dots, openActions }
+  },
+}
+</script>
+
