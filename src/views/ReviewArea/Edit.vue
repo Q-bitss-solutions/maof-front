0d9edf0bc0e4ada62aa01e1c1987d0f8c7eb6863
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:09:13 -0600

    add review area edit view

---
+++ src/views/ReviewArea/Edit.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="px-4 mt-10">
+    <arrow-back />
+    <title-bar title="Catálogos" subtitle="Áreas revisoras" />
+    <section class="px-4">
+      <form-review-area @submit="saveReviewArea" :reviewArea="app.reviewArea" edit-mode v-if="!app.loading" />
+    </section>
+  </main>
+</template>
+
+<script>
+import FormReviewArea from '../../components/ReviewArea/FormReviewArea.vue'
+import ArrowBack from '../../components/ArrowBack.vue'
+import TitleBar from '../../components/TitleBar.vue'
+import { updateReviewArea, fetchReviewAreaById } from '../../api/reviewArea'
+import { reactive } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default {
+  name: 'EditReviewArea',
+  components: {
+    FormReviewArea,
+    ArrowBack,
+    TitleBar,
+  },
+  setup() {
+    const route = useRoute()
+    const app = reactive({
+      reviewArea: {},
+      loading: true,
+    })
+    const getReviewAreaById = async () => {
+      app.loading = true
+      const { data } = await fetchReviewAreaById(route.params.reviewAreaId)
+      app.reviewArea = data
+      app.loading = false
+      console.log(app)
+    }
+    const saveReviewArea = async (reviewArea) => {
+      await updateReviewArea(reviewArea)
+    }
+
+    getReviewAreaById()
+
+    return {
+      app,
+      saveReviewArea,
+    }
+  },
+}
+</script>
