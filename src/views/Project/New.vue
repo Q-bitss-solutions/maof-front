358c868c38c2dc0441eea322669e98d6d9d8343b
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:08:13 -0600

    add new project view

---
+++ src/views/Project/New.vue
@@ -0,0 +1,38 @@
+<template>
+  <main class="px-4 mt-10">
+    <arrow-back />
+    <title-bar title="Catálogos" subtitle="Proyectos" />
+    <section class="px-4">
+      <form-project @submit="saveProject" />
+    </section>
+  </main>
+</template>
+
+<script>
+import FormProject from '../../components/Project/FormProject.vue'
+import { storeProject } from '../../api/project'
+import ArrowBack from '../../components/ArrowBack.vue'
+import TitleBar from '../../components/TitleBar.vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'EditProject',
+  components: {
+    FormProject,
+    ArrowBack,
+    TitleBar,
+  },
+  setup() {
+    const router = useRouter()
+    const saveProject = async (project) => {
+      await storeProject(project)
+      alert('Proyecto guardado con exito!')
+      router.push({ name: 'Projects' })
+    }
+
+    return {
+      saveProject,
+    }
+  },
+}
+</script>
