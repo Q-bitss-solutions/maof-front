a42c75243c74466dd4233cbbc20dfaa6f6cb81ae
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:00:05 -0600

    add button base web component

---
+++ src/components/ButtonBase.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    type="button"
+    class="bg-white text-red font-light text-lg py-2 px-6
+      block rounded cursor-pointer
+      border-red border-solid border-2
+      hover:text-white hover:bg-red
+      disabled:cursor-not-allowed
+    "
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonBase',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
