5ca0a633b5eb8f062a36e180fbc996555ae27b44
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-03-17 15:07:14 -0600

    add new contract view

---
+++ src/views/Contract/New.vue
@@ -0,0 +1,38 @@
+<template>
+  <main class="px-4 mt-10">
+    <arrow-back />
+    <title-bar title="Catálogos" subtitle="Areas Revisoras" />
+    <section class="px-4">
+      <form-contract @submit="saveContract" />
+    </section>
+  </main>
+</template>
+
+<script>
+import FormContract from '../../components/Contract/FormContract.vue'
+import ArrowBack from '../../components/ArrowBack.vue'
+import TitleBar from '../../components/TitleBar.vue'
+import { storeContract } from '../../api/contract'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'NewContract',
+  components: {
+    FormContract,
+    ArrowBack,
+    TitleBar,
+  },
+  setup() {
+    const router = useRouter()
+    const saveContract = async (contract) => {
+      await storeContract(contract)
+      alert('Contrato guardado con exito!')
+      router.push({ name: 'Contracts' })
+    }
+
+    return {
+      saveContract,
+    }
+  },
+}
+</script>
