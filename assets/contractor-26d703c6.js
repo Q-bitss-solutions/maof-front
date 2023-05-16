068e4d3df50d3b36c37595f2452b71d8c57492dc
Victor <out_vtorrecillasg@masnomina.com.mx>
2023-05-16 17:41:42 -0600

    deploy

--- assets/contractor-26d703c6.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./TitleBar-bd0b915b.js";const r=()=>t({url:"cat-contratista/",method:"GET"});export{r as f};
---
+++ assets/contractor-4a4b33ac.js
@@ -0,0 +1,1 @@
+import{f as t}from"./TitleBar-75e5b7b2.js";const r=()=>t({url:"cat-contratista/",method:"GET"});export{r as f};
