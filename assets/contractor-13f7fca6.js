20c0514322997301c8ae8f1ff69bced70dde88da
gustavo-agt <gustavo-agt@users.noreply.github.com>
2023-05-25 23:09:39 +0000

    Deploying to gh-pages from @ Q-bitss-solutions/maof-front@4e9397ac30d8d6838ef8c69349f35e33e0ab2d03 🚀

--- assets/contractor-13f7fca6.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./fetchApi-621ceb9d.js";const r=()=>t({url:"cat-contratista/",method:"GET"});export{r as f};
---
+++ assets/contractor-ad7695ae.js
@@ -0,0 +1,1 @@
+import{f as t}from"./fetchApi-d55dc87c.js";const r=()=>t({url:"cat-contratista/",method:"GET"});export{r as f};
