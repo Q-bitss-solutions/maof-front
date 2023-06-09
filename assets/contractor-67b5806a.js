94047188d9a91f21c38bcc5e9a3814a097687a58
victor-itg <victor-itg@users.noreply.github.com>
2023-06-09 05:43:41 +0000

    Deploying to gh-pages from @ Q-bitss-solutions/maof-front@492caeda280e4379fa8854d4f9605458e003738a 🚀

--- assets/contractor-67b5806a.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./fetchApi-fde5f25e.js";const r=()=>t({url:"cat-contratista/",method:"GET"});export{r as f};
---
+++ assets/contractor-f27317f3.js
@@ -0,0 +1,1 @@
+import{f as t}from"./fetchApi-9e5fb38d.js";const r=()=>t({url:"cat-contratista/",method:"GET"});export{r as f};
