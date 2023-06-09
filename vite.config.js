b9238f7736aa85cabd3cbb96fd6d29bddce63427
Victor Israel <victor.torrencillas@tecnoaplicada.mx>
2023-06-08 23:28:01 -0600

    ci: test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import 'dotenv/config'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
   ? process.env.VITE_PREFIX_URL
