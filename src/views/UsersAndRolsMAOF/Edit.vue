ab42cc5400a49b3b84bcbf8cb6d18d11fb0086f0
gustavo-agt <gustavo.ali@tecnoaplicada.mx>
2023-06-20 13:44:09 -0600

    arreglos usuarios y roles

--- src/views/UsersAndRolsMAOF/Edit.vue
+++ src/views/UsersAndRolsMAOF/Edit.vue
@@ -36,8 +36,6 @@ export default {
       app.loading = true
       const { data } = await fetchUserById(route.params.userRolMAOFId)
       app.userAndRols = data
-      console.log('data: ', data);
-      console.log('app.user: ', app.userAndRols);
       app.loading = false
     }
     const saveUserAndRols = async (userAndRol) => {
